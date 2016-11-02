95286ccbd49af1c829d8cad40c8c28f79f42030d
Tom <greenday@redbrick.dcu.ie>
2016-11-02 16:02:24 +0000

    Changed mistake

--- add-python3.py
+++ add-python3.py
@@ -4,4 +4,4 @@ total = 0
 for s in sys.argv[1:]:
    total += int(s)
 
-print total
+print(total)
--- tests/python3.sh
+++ tests/python3.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-python add_python2.py 1 2 3 | grep -q -w 6
+python3 add_python3.py 1 2 3 | grep -q -w 6
